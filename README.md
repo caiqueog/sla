7781c318d450094c17e911eda9881f1a7fd6ac7b
caiqueog <136257387+caiqueog@users.noreply.github.com>
2024-07-28 18:49:54 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+## Projeto do Bot do Discord
+
+Este é um projeto de um bot do Discord que utiliza a biblioteca `discord.js` para se conectar ao Discord e criar uma pagina de amigos.
+
+### Pré-requisitos
+
+- Node.js instalado no ambiente de desenvolvimento.
+
+### Instalação
+
+1. Clone este repositório em seu computador.
+2. No diretório raiz do projeto, execute o comando `npm install` para instalar as dependências.
+
+### Configuração
+
+Antes de executar o projeto, é necessário configurar o arquivo `config.js`. Siga os passos abaixo:
+
+1. Abra o arquivo `config.json` e edite conforme esta comentado.
+
+### Utilização
+
+1. No diretório raiz do projeto, execute o comando `node .` para iniciar o bot.
+2. O bot se conectará à sua conta do Discord e estará pronto para receber comandos.
+
+### Funcionalidades
+
+O bot possui as seguintes funcionalidades:
+
+- Renderização de Página: Renderiza a página principal do bot com as informações dos usuários, servidor e configurações.
+
+### Futuras Melhorias
+
+- Adição de sistema de configuração usando a API do OAuth do Discord.
+- Melhoria no layout da página principal.
+
+### Contribuições
+
+Contribuições e sugestões são sempre bem-vindas! Sinta-se à vontade para enviar um pull request ou abrir uma issue com sugestões de novas funcionalidades ou melhorias no código existente.
+
+### Observação
+
+É necessário baixar uma música, renomeá-la para "musica.mp3" e adicioná-la ao diretório "public/musica/" para que o bot possa reproduzi-la corretamente.
+
+![Exemplo do Bot](https://i.imgur.com/HDY5fWX.png)
---
+++ config.js
@@ -0,0 +1,55 @@
+﻿module.exports = {
+    server: {
+        /*caso deixe como true, aparecera no topo do site o icone do servidor, nome e um botão escrito entrar, caso não queira apenas deixe como false */
+        ativado: true,
+        id: '998334883665543218',
+        convite: 'https://discord.gg/147'
+    },
+    bot: {
+        token: 'MTE1MTk5MDY2OTk1OTc1NzgzNA.GU43zA.j1TUWtwlsuR0VC-SWLz2_CIwtfjwmGuQXKXYo8'
+    },
+    pagina: {
+        /*particulas que saem do mouse ao mexer*/
+        particulas: 'https://cdn.discordapp.com/emojis/998607318893723798.png',
+        /* false deixa o avatar quadrado, true deixa o avatar redondo*/
+        avatar_redondo: false,
+        /* recomendo deixar vazio para ficar da cor_fundo, wallpaper fica mt feio */
+        background: '',
+        /*cor do background*/
+        cor_fundo: '#000001',
+        /*titulo que fica na aba do site*/
+        titulo_pagina: '147 site'
+        },
+
+    /*COLOQUE O ID DO USUÁRIO E O MES CORRESPONDENTE AO BOOSTER DELE, CASO ELE NÃO TENHA BOOST COLOQUE null, siga esse exemplo*/
+    usuarios: {
+      '1080098617924255765': null,
+      '960657593318141992': 'mes_6',
+      '687022032726392839': 'mes_3',
+      '122102409781575681': 'mes_24'
+
+    },
+    /* não mexa */
+    badges: {
+        nitro: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordnitro.svg',
+        PremiumEarlySupporter: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordearlysupporter.svg',
+        ActiveDeveloper: 'https://raw.githubusercontent.com/efeeozc/discord-badges/6c54af4b871af1e6daef9722f02257af18446129/svg/activedeveloper.svg',
+        HypeSquadOnlineHouse1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbravery.svg',
+        HypeSquadOnlineHouse2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbrilliance.svg',
+        HypeSquadOnlineHouse3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbalance.svg',
+        VerifiedDeveloper: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordbotdev.svg',
+        novo_nick: 'https://github.com/mezotv/discord-badges/blob/main/assets/username.png?raw=true'
+    },
+    boost: {
+        mes_1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost1.svg',
+        mes_2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost2.svg',
+        mes_3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost3.svg',
+        mes_6: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost4.svg',
+        mes_9: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost5.svg',
+        mes_12: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost6.svg',
+        mes_15: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost7.svg',
+        mes_18: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost8.svg',
+        mes_24: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost9.svg'
+    },
+    
+};
